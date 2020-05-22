80538823d6b6eca62851127133c6c421776e376a
Vanya Dimitrova <vanyad@gmail.com>
2020-05-22 11:17:43 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({}, {})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
